18ac4cd0446fd064d6b0c1a718d63a08a2b5343f
Tejaswini0910 <129576681+Tejaswini0910@users.noreply.github.com>
2024-06-01 19:12:00 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,12 @@
 
+import { extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+});
+
+export default customTheme;
+
