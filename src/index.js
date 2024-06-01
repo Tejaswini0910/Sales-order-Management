d94394499505aca893cfa46e9b6c92d60d339f63
Tejaswini0910 <129576681+Tejaswini0910@users.noreply.github.com>
2024-06-01 19:11:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import ThemeProvider from './contexts/ThemeContext';
+import AuthProvider from './contexts/AuthContext';
+import App from './App';
+import './index.css';
 
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <ChakraProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </ChakraProvider>
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
