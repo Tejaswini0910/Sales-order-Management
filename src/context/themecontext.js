4c3028edb7e69e0b977f55f8cc781ed7adbf2cf7
Tejaswini0910 <129576681+Tejaswini0910@users.noreply.github.com>
2024-06-01 19:08:12 +0530

    Update themecontext.js

--- src/context/themecontext.js
+++ src/context/themecontext.js
@@ -1,7 +1,34 @@
-import SaleOrderList from '../components/SaleOrderList';
+// src/contexts/ThemeContext.js
+import { createContext, useContext, useEffect, useState } from 'react';
+import { ChakraProvider, extendTheme, useColorMode } from '@chakra-ui/react';
 
-const CompletedSaleOrders = () => {
-  return <SaleOrderList status="completed" />;
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  const customTheme = extendTheme({
+    config: {
+      initialColorMode: theme,
+      useSystemColorMode: false,
+    },
+  });
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
+    </ThemeContext.Provider>
+  );
 };
 
-export default CompletedSaleOrders;
+export const useTheme = () => useContext(ThemeContext);
+
+export default ThemeProvider;
